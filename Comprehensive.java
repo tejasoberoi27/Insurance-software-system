27cf0335549002b9b2009af2ab63d5413a029b3e
Tejas Oberoi <35334979+tejasoberoi27@users.noreply.github.com>
2020-09-01 17:30:28 +0530

    Add files via upload
    Added all files related to assignment.

---
+++ Comprehensive.java
@@ -0,0 +1,11 @@
+package lab3;
+
+import java.util.Date;
+
+public final class Comprehensive extends insurance_policy{
+
+	public Comprehensive(Date expiry_date) {
+		super(50, 80, expiry_date,"Comprehensive");
+	}
+	
+}
